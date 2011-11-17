0ddb94860b83c4a7639c7b20516619bc61872bd1
Sebastian Fabian <sjlfabian@gmail.com>
2011-11-18 00:33:10 +0100

    refactored centering in two other frames

--- src/kanjieditor/KanjiEditor.java
+++ src/kanjieditor/KanjiEditor.java
@@ -74,12 +74,7 @@ public class KanjiEditor {
 		proppanel.add(butpanel);
 		
 		frame.pack();
-	    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-	    int w = frame.getSize().width;
-	    int h = frame.getSize().height;
-	    int x = (dim.width-w)/2;
-	    int y = (dim.height-h)/2;
-	    frame.setLocation(x, y);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 
