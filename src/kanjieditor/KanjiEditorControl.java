7582205d60a6ebb4d1cf04c63ff88a2760142b29
Sebastian Fabian <sjlfabian@gmail.com>
2011-11-23 17:54:39 +0100

    forgot to add some new files

---
+++ src/kanjieditor/KanjiEditorControl.java
@@ -0,0 +1,24 @@
+package kanjieditor;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import vocab.Kanji;
+
+public class KanjiEditorControl {
+	
+	public static void saveToFile(File file, ArrayList<Kanji> klist) throws FileNotFoundException, IOException{
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+		out.writeObject(klist);
+	}
+	
+	public static ArrayList<Kanji> loadFromFile(File file) throws FileNotFoundException, IOException, ClassNotFoundException{
+		ObjectInputStream in=new ObjectInputStream(new FileInputStream(file));
+		ArrayList<Kanji> newlist =(ArrayList<Kanji>)in.readObject();
+		return newlist;
+	}
+}
---
+++ src/wordeditor/WordEditorControl.java
@@ -0,0 +1,24 @@
+package wordeditor;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import vocab.Word;
+
+public class WordEditorControl {
+	
+	public static void saveToFile(File file, ArrayList<Word> wlist) throws FileNotFoundException, IOException{
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+		out.writeObject(wlist);
+	}
+	
+	public static ArrayList<Word> loadFromFile(File file) throws FileNotFoundException, IOException, ClassNotFoundException{
+		ObjectInputStream in=new ObjectInputStream(new FileInputStream(file));
+		ArrayList<Word> newlist =(ArrayList<Word>)in.readObject();
+		return newlist;
+	}
+}
